57edd7d8f93497d157f9738332e47b660965bc62
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-03-29 17:35:05 +0300

    Update PolynomialTests.cs

--- Day6Task3-4.Tests/PolynomialTests.cs
+++ Day6Task3-4.Tests/PolynomialTests.cs
@@ -8,32 +8,32 @@ namespace NET.S._2018.Haiduk._06
         [Test]
         public void PolynomialTest1()
         {
-            Polynomial p1 = new Polynomial(1.234, new int[] { 5, 6, 7, 8 });
-            Polynomial p2 = new Polynomial(1.234, p1.Coefficients);            
+            Polynomial p1 = new Polynomial(new double[] { 5, 6, 7, 8 });
+            Polynomial p2 = new Polynomial(new double[] { 5, 6, 7, 8 });            
             Assert.IsTrue(p1.Equals(p2));            
         }
                 
         [Test]
         public void PolynomialTest2()
         {
-            Polynomial p1 = new Polynomial(double.MaxValue, new int[] { 10, 11, 12 });
-            Polynomial p2 = new Polynomial(double.MaxValue, new int[] { 10, 11, 12 });
+            Polynomial p1 = new Polynomial(new double[] { 10, 11, 12 });
+            Polynomial p2 = new Polynomial(new double[] { 10, 11, 12 });
             Assert.IsTrue(p1 == p2);
         }
 
         [Test]
         public void PolynomialTest3()
         {
-            Polynomial p1 = new Polynomial(double.MinValue, new int[] { 10, 11, 12 });
-            Polynomial p2 = new Polynomial(double.NaN, new int[] { 10, 11, 12 });
+            Polynomial p1 = new Polynomial(new double[] { 10, 11, 12 });
+            Polynomial p2 = new Polynomial(new double[] { 10, 11, 12 });
             Assert.IsTrue(p1 != p2);
         }
 
-        [TestCase(new int[] { 2, 3, 4, 5 }, ExpectedResult = "2 + 3 * x + 4 * x ^ 2 + 5 * x ^ 3")]
-        [TestCase(new int[] { 14, 22, 9, 53, 150 }, ExpectedResult = "14 + 22 * x + 9 * x ^ 2 + 53 * x ^ 3 + 150 * x ^ 4")]
-        public string PolynomialToStringTest(int[] coefficients)
+        [TestCase(new double[] { 2, 3, 4, 5 }, ExpectedResult = "2 + 3 * x + 4 * x ^ 2 + 5 * x ^ 3")]
+        [TestCase(new double[] { 14, 22, 9, 53, 150 }, ExpectedResult = "14 + 22 * x + 9 * x ^ 2 + 53 * x ^ 3 + 150 * x ^ 4")]
+        public string PolynomialToStringTest(double[] coefficients)
         {
-            Polynomial p1 = new Polynomial(double.Epsilon, coefficients);
+            Polynomial p1 = new Polynomial(coefficients);
             return p1.ToString();
         }
     }
