9f8702511854a4fafb6124883f4a677192054046
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-06 17:02:49 +0300

    Update BubbleSortClassTests.cs

--- Day6Task3-4.Tests/BubbleSortClassTests.cs
+++ Day6Task3-4.Tests/BubbleSortClassTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using NUnit.Framework;
 
 namespace NET.S._2018.Haiduk._06
@@ -7,7 +7,7 @@ namespace NET.S._2018.Haiduk._06
     public class BubbleSortClassTests
     {
         [TestCase(null)]
-        public void BubbleSortTest_AcceptsEmptyArray_ThrowsException(int[][] array) => Assert.Throws<ArgumentNullException>(() => BubbleSortClass.BubbleSort(array, new SortByMaxAscending()));
+        public void BubbleSortTest_AcceptsEmptyArray_ThrowsException(int[][] array) => Assert.Throws<ArgumentNullException>(() => BubbleSortClass.BubbleSort(array, null));
 
         [Test]
         public void BubbleSortTest_AcceptsNonJaggedArray_ReturnsGivenArray()
@@ -16,9 +16,9 @@ namespace NET.S._2018.Haiduk._06
             int[][] array2 = new int[1][];
             int[][] array3 = new int[1][];
             int[][] array4 = new int[1][];
-            BubbleSortClass.BubbleSort(array2, new SortByMaxAscending());
-            BubbleSortClass.BubbleSort(array3, new SortByMinAscending());
-            BubbleSortClass.BubbleSort(array4, new SortBySumAscending());
+            BubbleSortClass.BubbleSort(array2, null);
+            BubbleSortClass.BubbleSort(array3, null);
+            BubbleSortClass.BubbleSort(array4, null);
             CollectionAssert.AreEqual(array1, array2);
             CollectionAssert.AreEqual(array1, array3);
             CollectionAssert.AreEqual(array1, array4);
@@ -28,8 +28,8 @@ namespace NET.S._2018.Haiduk._06
         public void BubbleSortByMaxOfRowElementsDescendingTest()
         {
             int[][] array1 = new int[3][] { new int[] { 5, 12, -3 }, new int[] { 0, 3, 14 }, new int[] { 7, 8, 9 } };
-            int[][] sorted = new int[3][] { new int[] { 0, 3, 14 }, new int[] { 5, 12, -3 }, new int[] { 7, 8, 9 } };
-            BubbleSortClass.BubbleSort(array1, new SortByMaxDescending());
+            int[][] sorted = new int[3][] { new int[] { 0, 3, 14 }, new int[] { 5, 12, -3 }, new int[] { 7, 8, 9 } };            
+            BubbleSortClass.BubbleSort(array1, BubbleSortClass.SortByMaxDescending);
             CollectionAssert.AreEqual(sorted, array1);
         }
 
@@ -38,7 +38,7 @@ namespace NET.S._2018.Haiduk._06
         {
             int[][] array1 = new int[3][] { new int[] { 6, 18, -10 }, new int[] { 10, 10, 12, -7 }, new int[] { 20 } };
             int[][] sorted = new int[3][] { new int[] { 10, 10, 12, -7 }, new int[] { 6, 18, -10 }, new int[] { 20 } };
-            BubbleSortClass.BubbleSort(array1, new SortByMaxAscending());
+            BubbleSortClass.BubbleSort(array1, BubbleSortClass.SortByMaxAscending);
             CollectionAssert.AreEqual(sorted, array1);
         }
 
@@ -47,7 +47,7 @@ namespace NET.S._2018.Haiduk._06
         {
             int[][] array1 = new int[3][] { new int[] { -15, 26 }, new int[] { 4, 55, 8, -90 }, new int[] { 1, 3, -10, -7 } };
             int[][] sorted = new int[3][] { new int[] { 1, 3, -10, -7 }, new int[] { -15, 26 }, new int[] { 4, 55, 8, -90 } };
-            BubbleSortClass.BubbleSort(array1, new SortByMinDescending());
+            BubbleSortClass.BubbleSort(array1, BubbleSortClass.SortByMinDescending);
             CollectionAssert.AreEqual(sorted, array1);
         }
 
@@ -56,7 +56,7 @@ namespace NET.S._2018.Haiduk._06
         {
             int[][] array1 = new int[4][] { new int[] { -4, 0 }, new int[] { 10, 10, 12, -7 }, new int[] { 0, 0, 0, 0 }, new int[] { 19, 19, 20 } };
             int[][] sorted = new int[4][] { new int[] { 10, 10, 12, -7 }, new int[] { -4, 0 }, new int[] { 0, 0, 0, 0 }, new int[] { 19, 19, 20 } };
-            BubbleSortClass.BubbleSort(array1, new SortByMinAscending());
+            BubbleSortClass.BubbleSort(array1, BubbleSortClass.SortByMinAscending);
             CollectionAssert.AreEqual(sorted, array1);
         }
 
@@ -65,7 +65,7 @@ namespace NET.S._2018.Haiduk._06
         {
             int[][] array1 = new int[3][] { new int[] { 1, 2, 3, -8 }, new int[] { 5, -5, 12, -9 }, new int[] { 33, 22, -40, 10 } };
             int[][] sorted = new int[3][] { new int[] { 33, 22, -40, 10 }, new int[] { 5, -5, 12, -9 }, new int[] { 1, 2, 3, -8 } };
-            BubbleSortClass.BubbleSort(array1, new SortBySumDescending());
+            BubbleSortClass.BubbleSort(array1, BubbleSortClass.SortBySumDescending);
             CollectionAssert.AreEqual(sorted, array1);
         }
 
@@ -74,7 +74,7 @@ namespace NET.S._2018.Haiduk._06
         {
             int[][] array1 = new int[4][] { new int[] { -4, 0 }, new int[] { 10, 10, 12, -7 }, new int[] { 0, 0, 0, 0 }, new int[] { 19, 19, 20 } };
             int[][] sorted = new int[4][] { new int[] { -4, 0 }, new int[] { 0, 0, 0, 0 }, new int[] { 10, 10, 12, -7 }, new int[] { 19, 19, 20 } };
-            BubbleSortClass.BubbleSort(array1, new SortBySumAscending());
+            BubbleSortClass.BubbleSort(array1, BubbleSortClass.SortBySumAscending);
             CollectionAssert.AreEqual(sorted, array1);
         }
     }
